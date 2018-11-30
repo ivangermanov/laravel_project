9b69d31f7d07ed1b1af62328b6ba54ca0aca70e2
Germanov,Ivan I.P <i.germanov@student.fontys.nl>
2018-11-30 17:34:11 +0100

    Fixed template of layout, carousel not showing bug, CSRF token warning

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,21 +1,24 @@
-</html>
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{asset('css/app.css')}}">
-        <title>@yield('title')</title>
-    </head>
-    <body>
-        @include('inc.navbar')
-        <div class="container">
-            @section('sidebar')
-                This is the master sidebar.
-            @show
-            <div class="container">
-                @yield('content')
-            </div>
-        </div>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>@yield('title')</title>
+    <!-- CSRF token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- JavaScript -->
+    <script type="text/javascript" src="{{asset('js/app.js')}}"></script>
+    <!-- CSS -->
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+</head>
+
+<body>
+    @include('inc.navbar')
+    <div class="container">
+        @yield('content')
+    </div>
+    @include('inc.footer')
+</body>
+
 </html>
