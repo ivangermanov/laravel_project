860f0499ae7c0df88a96485d2254a7226a927bdf
Germanov,Ivan I.P <i.germanov@student.fontys.nl>
2018-11-30 17:43:26 +0100

    Created Breed model

---
+++ app/Breed.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Breed extends Model
+{
+    // protected $table = 'breeds';
+    // protected $primaryKey = 'id';
+    // If primary key not incrementing or non-numeric, set
+    // protected $incrementing = false;
+    // If primary key not an integer
+    // protected $keyType = string;
+    // If no timestamps
+    // public $timestamps = false;
+}
