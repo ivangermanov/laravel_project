aec83d87e92bf1dc1946ea26cedcefc7d3f93fea
Dan Niculae <d.niculae@student.fontys.nl>
2018-12-02 19:08:45 +0100

    mistakes werre made

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,16 +8,11 @@ class PagesController extends Controller
     public function index()
     {
         // Returns view of index page
-<<<<<<< HEAD
-        $popularBreeds = Breed::orderBy('visits')->take(3)->get();
+        $popularBreeds = Breed::orderBy('visits', 'desc')->take(3)->get();
         $recentBreeds = Breed::orderBy('created_at')->take(6)->get();
         return view('pages.index')
             ->with('popularBreeds', $popularBreeds)
             ->with('recentBreeds', $recentBreeds);
-=======
-        $popularBreeds = Breed::orderBy('visits', 'desc')->take(3)->get();
-        return view('pages.index')->with('popularBreeds', $popularBreeds);
->>>>>>> 7a0233c56c3dfca46aa3bcab499a707bd3102de4
     }
 
     public function about()
