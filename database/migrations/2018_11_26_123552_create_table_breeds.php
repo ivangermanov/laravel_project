a4ba7c8b3053e383bc6856d12fe88392f6d9df0f
Dan Niculae <d.niculae@student.fontys.nl>
2018-12-16 16:34:05 +0100

    Breed name is now unique

--- database/migrations/2018_11_26_123552_create_table_breeds.php
+++ database/migrations/2018_11_26_123552_create_table_breeds.php
@@ -16,7 +16,7 @@ class CreateTableBreeds extends Migration
     {
         Schema::create('breeds', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('breed');
+            $table->string('breed')->unique();
             $table->unsignedDecimal('height', 3, 2);
             $table->unsignedTinyInteger('weight');
             $table->text('history');
